7a3d5722fee2f9eccd6bd81d249a4ab3377ddd0c
softir <elisabetta.giani@inaf.it>
2019-07-09 17:04:43 +0200

    Updated the start/stop scripts to include device properties configuration and the CspSubarray start.

--- start_prototype.sh
+++ start_prototype.sh
@@ -14,6 +14,11 @@ FILE=jive
 #check if TANGO DB is up
 tango_admin --ping-database	
 if [ $? -eq 0 ]; then 
+	# configure device properties 
+	sleep 2 
+	echo "Configuring Class/Device properties"
+	python csplmc/configureDevices.py 
+	sleep 2
 	# TANGO DB is running -> check for CbfTestMaster device existence
 	tango_admin --check-device mid_csp_cbf/sub_elt/master  >> /dev/null
         if [ $? -eq 0 ]; then
@@ -36,17 +41,30 @@ if [ $? -eq 0 ]; then
            echo "CspMaster device already register in the TANGO DB"
         else
            echo  "Adding the CspMaster device to DB"
-	   tango_admin --add-server CspMaster/csp CspMaster mid_csp/sub/master
+	   tango_admin --add-server CspMaster/csp CspMaster mid_csp/elt/master
 	fi
 	echo "Starting the CspMaster device"
 	python csplmc/CspMaster/CspMaster/CspMaster.py csp >/dev/null 2>&1 &
 	if [ $? -lt 0 ]; then
             echo "CspMaster device failure"
         fi 	    
+	# check for CspSubarray1 device existence
+	tango_admin --check-device mid_csp/elt/subarray_01
+        if [ $? -eq 0 ]; then
+           echo "CspSubarray1 device already register in the TANGO DB"
+        else
+           echo  "Adding the CspSubarray1 device to DB"
+	   tango_admin --add-server CspSubarray/sub1 CspSubarray mid_csp/elt/subarray_01
+	fi
+	echo "Starting the CspSubarray1 device"
+	python csplmc/CspSubarray/CspSubarray/CspSubarray.py sub1 >/dev/null 2>&1 &
+	if [ $? -lt 0 ]; then
+            echo "CspSubarray1 device failure"
+        fi 	    
 	# check for jive tool 
         command_line="which $FILE"
 	return=`$command_line`
-	#echo $return
+	echo $return
         if [ $? -gt 0 ]; then
             echo "Jive tool not found"	
             # jive tool found. Go to run it
